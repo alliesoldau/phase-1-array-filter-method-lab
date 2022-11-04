160c4851f0a58734eece7e99b4c8c65c751f0522
Allie Soldau <alliesoldau@gmail.com>
2022-11-04 07:31:45 -0400

    commit tree c7ab70cad6e21f608ed9d6ccff7b348f9484ae98

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(names, comparator) {
+    let matchingList = names.filter(element => element.toLowerCase() === comparator.toLowerCase())
+    return matchingList
+  }
+
+function fuzzyMatch(driver, letters) {
+    let letterLength = letters.length
+    let letterMatches = driver.filter(function(name) {
+        return (name.slice(0, letterLength) === letters) // use slice to grab just the part of the drivers name that is the length of the letters
+    })
+    return letterMatches
+}
+
+function matchName(driver, string) {
+    return driver.filter(info => info.name === string)
+}
